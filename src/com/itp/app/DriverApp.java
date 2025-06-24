58958d7c482cc748be2e48c2abf73944f92b1386
uysolanki <uysolanki@gmail.com>
2025-06-24 13:29:36 +0530

    sprinf core

--- src/com/itp/app/DriverApp.java
+++ src/com/itp/app/DriverApp.java
@@ -2,18 +2,17 @@ package com.itp.app;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.itp.model.Car;
 import com.itp.model.Engine;
 
 public class DriverApp {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext beanFactory = new ClassPathXmlApplicationContext("resources/application-context.xml");
-		Engine engine1=beanFactory.getBean("e1",Engine.class);
-		System.out.println(engine1);
+		Car car1=beanFactory.getBean("c1",Car.class);
+		System.out.println(car1);
 		
-		Engine engine2=beanFactory.getBean("e2",Engine.class);
-		System.out.println(engine2);
-
+		Car car2=beanFactory.getBean("c2",Car.class);
+		System.out.println(car2);
 	}
-
 }
